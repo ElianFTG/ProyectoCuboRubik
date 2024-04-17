727aef205c19732eca7b8d09266d3628e4322129
Elian Tito <eliantito234@gmail.com>
2024-04-17 00:37:02 -0400

    Correcion de movimientos del cubo y cambios de nomenclatura

--- Cubo_rubik.py
+++ Cubo_rubik.py
@@ -8,7 +8,7 @@ class CuboRubik:
         self.derecha= [[]*3, []*3, []*3]
         self.abajo = [[]*3, []*3, []*3]
         self.atras = [[]*3, []*3, []*3]
-        self.cubo = [self.arriba, self.frente, self.izquierda, self.derecha, self.abajo, self.atras]
+        self.cubo = [self.arriba, self.frente, self.izquierda, self.derecha, self.atras, self.abajo]
 
     def mostrar_sector(self, lado):
         for i in range(len(lado)):
@@ -22,7 +22,7 @@ class CuboRubik:
         return False
 
     def mostrar_cubo(self):
-        lados = ['arriba', 'frente', 'izquierda', 'derecha', 'abajo', 'atrás']
+        lados = ['arriba', 'frente', 'izquierda', 'derecha', 'atrás', 'abajo']
         for indice in range(len(lados)):
             print(lados[indice])
             self.mostrar_sector(self.cubo[indice])
@@ -41,66 +41,93 @@ class CuboRubik:
             self.cubo[recorrido_caras][casillas_cubo] = lineas[indice].split()
             casillas_cubo += 1
 
-    def girar_cara_horario(self, cara):
-        cara[:] = [list(reversed(col)) for col in zip(*cara)]
-
-    def girar_cara_antihorario(self, cara):
-        cara[:] = [list(col[::-1]) for col in zip(*cara)]
-    
-    def girar_arriba_horario(self):
-        self.girar_cara_horario(self.arriba)
-        temp = [self.frente[0][:], self.izquierda[0][:], self.atras[0][:], self.derecha[0][:]]
-        self.frente[0], self.izquierda[0], self.atras[0], self.derecha[0] = temp[1], temp[2], temp[3], temp[0]
-
-    def girar_arriba_antihorario(self):
-        self.girar_cara_antihorario(self.arriba)
-        temp = [self.frente[0][:], self.izquierda[0][:], self.atras[0][:], self.derecha[0][:]]
-        self.frente[0], self.izquierda[0], self.atras[0], self.derecha[0] = temp[3], temp[0], temp[1], temp[2]
-
-    def girar_frente_horario(self):
-        self.girar_cara_horario(self.frente)
-        temp = [self.arriba[2][:], list(reversed(self.izquierda[2])), self.abajo[0][:], list(reversed(self.derecha[0]))]
-        self.arriba[2], self.izquierda[2], self.abajo[0], self.derecha[0] = temp[1], temp[2], temp[3], temp[0]
-
-    def girar_frente_antihorario(self):
-        self.girar_cara_antihorario(self.frente)
-        temp = [self.arriba[2][:], list(reversed(self.izquierda[2])), self.abajo[0][:], list(reversed(self.derecha[0]))]
-        self.arriba[2], self.izquierda[2], self.abajo[0], self.derecha[0] = temp[3], temp[0], temp[1], temp[2]
-
-    def girar_izquierda_horario(self):
-        self.girar_cara_horario(self.izquierda)
-        temp = [self.arriba[:][0], self.atras[::-1][0], self.abajo[:][0], self.frente[:][0]]
-        self.arriba[:][0], self.atras[::-1][0], self.abajo[:][0], self.frente[:][0] = temp[1], temp[2], temp[3], temp[0]
-
-    def girar_izquierda_antihorario(self):
-        self.girar_cara_antihorario(self.izquierda)
-        temp = [self.arriba[:][0], self.atras[::-1][0], self.abajo[:][0], self.frente[:][0]]
-        self.arriba[:][0], self.atras[::-1][0], self.abajo[:][0], self.frente[:][0] = temp[3], temp[0], temp[1], temp[2]
-
-    def girar_derecha_horario(self):
-        self.girar_cara_horario(self.derecha)
-        temp = [self.arriba[:][2], self.frente[:][2], self.abajo[:][2], self.atras[::-1][2]]
-        self.arriba[:][2], self.frente[:][2], self.abajo[:][2], self.atras[::-1][2] = temp[3], temp[0], temp[1], temp[2]
-
-    def girar_derecha_antihorario(self):
-        self.girar_cara_antihorario(self.derecha)
-        temp = [self.arriba[:][2], self.frente[:][2], self.abajo[:][2], self.atras[::-1][2]]
-        self.arriba[:][2], self.frente[:][2], self.abajo[:][2], self.atras[::-1][2] = temp[1], temp[2], temp[3], temp[0]
-
-    def girar_abajo_horario(self):
-        self.girar_cara_horario(self.abajo)
-        temp = [self.frente[2][:], self.derecha[2][:], self.atras[2][:], self.izquierda[2][:]]
-        self.frente[2], self.derecha[2], self.atras[2], self.izquierda[2] = temp[1], temp[2], temp[3], temp[0]
-
-    def girar_abajo_antihorario(self):
-        self.girar_cara_antihorario(self.abajo)
-        temp = [self.frente[2][:], self.derecha[2][:], self.atras[2][:], self.izquierda[2][:]]
-        self.frente[2], self.derecha[2], self.atras[2], self.izquierda[2] = temp[3], temp[0], temp[1], temp[2]
-
-    def girar_atras_horario(self):
-        self.girar_cara_horario(self.atras)
-        temp = [self.arriba[::-1][2], self.derecha[::-1][2], self.abajo[::-1][2], self.izquierda[::-1][2]]
-        self.arriba[::-1][2], self.derecha[::-1][2], self.abajo[::-1][2], self.izquierda[::-1][2] = temp[3]
+    def Up(self):
+        self.izquierda[0], self.frente[0], self.derecha[0],self.atras[0] = self.frente[0],self.derecha[0],self.atras[0],self.izquierda[0]
         
+
+    def Up_prima(self):
+        self.derecha[0], self.atras[0],self.izquierda[0],self.frente[0] = self.frente[0],self.derecha[0],self.atras[0],self.izquierda[0]
+        # Funciones de giro en sentido horario
+    def Front(self):
+        temp_up = self.arriba[-1][:]
+        temp_down = self.abajo[0][:]
+
+
+        for i in range(3):
+            self.arriba[-1][i] = self.izquierda[i][-1]
+            self.abajo[0][i] = self.derecha[i][0]
+            self.izquierda[i][-1] = temp_down[i]
+            self.derecha[i][0] = temp_up[i]
+
+        self.frente = list(map(list, zip(*self.frente[::-1])))
+
+        
+
+    def transpuesta(self, matriz):
+        repositorio = [[],[],[]]
+        for fila in range(3):
+            for columna in range(3):
+                repositorio[fila].append(matriz[columna][fila])
+        return repositorio
+        
+        
+
+    def Left(self):
+        for i in range(3):
+            self.arriba[i][0], self.atras[2-i][2], self.abajo[i][0], self.frente[i][0] = \
+                self.atras[2-i][2], self.abajo[i][0], self.frente[i][0], self.arriba[i][0]
+        self.izquierda = list(map(list, zip(*self.izquierda[::-1])))
+        
+
+    def Right(self):
+        for i in range(3):
+            self.arriba[i][2], self.atras[2-i][0], self.abajo[i][2], self.frente[i][2] = \
+                self.frente[i][2], self.arriba[i][2], self.atras[2-i][0], self.abajo[i][2]
+        self.derecha = list(map(list, zip(*self.derecha[::-1])))
+        
+
+    def Back_prima(self):
+        for i in range(3):
+            self.arriba[0][i], self.izquierda[2-i][0], self.abajo[2][i], self.derecha[2-i][2] = \
+                self.izquierda[2-i][0], self.abajo[2][i], self.derecha[2-i][2], self.arriba[0][i]
+        self.atras = list(map(list, zip(*self.atras[::-1])))
+
+        
+
+    def Down(self):
+        self.frente[2],self.derecha[2],self.atras[2],self.izquierda[2] = self.izquierda[2], self.frente[2],self.derecha[2], self.atras[2]
+        
+
+    # Funciones de giro en sentido antihorario
+    def Front_prima(self):
+        for i in range(3):
+            self.arriba[2][i], self.derecha[i][0], self.abajo[0][i], self.izquierda[i][2] = \
+                self.derecha[i][0], self.abajo[0][i], self.izquierda[i][2], self.arriba[2][i]
+        self.frente = list(map(list, zip(*self.frente[::-1])))
+
+        
+
+    def Left_prima(self):
+        for i in range(3):
+            self.arriba[i][0], self.frente[i][0], self.abajo[i][0], self.atras[2-i][2] = \
+                self.frente[i][0], self.abajo[i][0], self.atras[2-i][2], self.arriba[i][0]
+        self.izquierda = list(map(list, zip(*self.izquierda[::-1])))
+
+    def Right_prima(self):
+        for i in range(3):
+            self.arriba[i][2], self.frente[i][2], self.abajo[i][2], self.atras[2-i][0] = \
+                self.atras[2-i][0], self.arriba[i][2], self.frente[i][2], self.abajo[i][2]
+        self.derecha = list(map(list, zip(*self.derecha[::-1])))
+
+    def Back(self):
+        for i in range(3):
+            self.arriba[0][i], self.derecha[2-i][2], self.abajo[2][i], self.izquierda[2-i][0] = \
+                self.derecha[2-i][2], self.abajo[2][i], self.izquierda[2-i][0], self.arriba[0][i]
+        self.atras = list(map(list, zip(*self.atras[::-1])))
+
+    def Down_prima(self):
+        self.frente[2],self.derecha[2],self.atras[2],self.izquierda[2] = self.derecha[2], self.atras[2],self.izquierda[2], self.frente[2]
+
+    
 
 
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from Cubo_rubik import *
 from BusquedasGrafos import *
 
+def mover_piezas():
+    cubo = CuboRubik() 
+    cubo
+    pass
 
 
+matriz_generica = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
 cubo = CuboRubik()
-cubo2 = CuboRubik()
 cubo.insertar_datos_cubo("lecturacubo.txt")
-cubo2.insertar_datos_cubo("lecturacubo.txt")
-print(cubo == cubo2)
+#cubo2.insertar_datos_cubo("lecturacubo.txt")
+cubo.Down_prima()
+cubo.mostrar_cubo()
 
